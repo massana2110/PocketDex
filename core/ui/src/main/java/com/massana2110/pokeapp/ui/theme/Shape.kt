04b3877687587e1c66a597d7c82a210ada8de892
David Massana <david.massana.10@gmail.com>
2026-04-11 01:03:23 -0600

    💄 Create material theme for pocketdex app
    Also, add the respective resources such as fonts, string values, drawables and create the common components

---
+++ core/ui/src/main/java/com/massana2110/pokeapp/ui/theme/Shape.kt
@@ -0,0 +1,14 @@
+package com.massana2110.pokeapp.ui.theme
+
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+internal val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(12.dp),
+    large = RoundedCornerShape(16.dp),
+    extraLarge = CircleShape // Rounded full corners
+)
--- core/ui/src/main/java/com/massana2110/pokeapp/ui/theme/Type.kt
+++ core/ui/src/main/java/com/massana2110/pokeapp/ui/theme/Type.kt
@@ -2,33 +2,86 @@ package com.massana2110.pokeapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.massana2110.pokeapp.core.ui.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
+internal val montserratFontFamily = FontFamily(
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_semi_bold, FontWeight.SemiBold),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
+
+/**
+ * Typography configuration for the app design system.
+ *
+ * Mapping from Pokédex Figma design typography to Material3 Typography
+ * - titleS = TitleLarge (20sp/34sp, Bold)
+ * - SemiBold/Variant M = TitleMedium (18sp/26sp, SemiBold)
+ * - SemiBold/Variant S = TitleSmall (16sp/26sp, SemiBold)
+ * - subtitle = BodyLarge (20sp/32sp, Regular)
+ * - Medium/Variant S = BodyMedium (14sp/20sp, Medium)
+ * - bodyS = BodySmall (12sp/16sp, Medium )
+ * - Bold/Bold 16px = LabelLarge (16sp/24sp, Bold)
+ * - Bold / Bold 14px = LabelMedium (14sp/20sp, Bold)
+ * - Regular/Regular 12px = LabelSmall (12sp/16sp, Regular)
+ */
+internal val PocketDexTypography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 34.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        lineHeight = 26.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 26.sp,
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = montserratFontFamily,
         fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        lineHeight = 32.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
     )
-    */
 )
