e5a55b43c4c329ae241ce2b31b89f7bbafedab6e
David Massana <david.massana.10@gmail.com>
2026-04-11 12:47:24 -0600

    📦 Add domain module
    For manage domain models and usecases

---
+++ core/domain/src/main/java/com/massana2110/pokeapp/core/domain/repository/PokemonRepository.kt
@@ -0,0 +1,7 @@
+package com.massana2110.pokeapp.core.domain.repository
+
+import com.massana2110.pokeapp.core.domain.model.Pokemon
+
+interface PokemonRepository {
+    suspend fun getPokemonGeneration1(): Result<List<Pokemon>>
+}
