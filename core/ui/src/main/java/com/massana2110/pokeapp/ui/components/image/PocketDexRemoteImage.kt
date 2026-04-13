1554cf3d379c98c23b63c6df968991ab06f4940d
David Massana <david.massana.10@gmail.com>
2026-04-12 20:36:38 -0600

    💄 Add UI components for pokemon detail screen

--- core/ui/src/main/java/com/massana2110/pokeapp/ui/components/image/PocketDexRemoteImage.kt
+++ core/ui/src/main/java/com/massana2110/pokeapp/ui/components/image/PocketDexRemoteImage.kt
@@ -2,16 +2,25 @@ package com.massana2110.pokeapp.ui.components.image
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import coil3.compose.AsyncImage
 
+/**
+ * A composable that displays an image from a remote URL.
+ * @param modifier The modifier to be applied to the image.
+ * @param scale The scale to apply to the image.
+ * @param imageUrl The URL of the image to display.
+ */
 @Composable
 fun PocketDexRemoteImage(
     modifier: Modifier = Modifier,
+    scale: ContentScale = ContentScale.Fit,
     imageUrl: String
 ) {
     AsyncImage(
         modifier = modifier,
         model = imageUrl,
+        contentScale = scale,
         contentDescription = null
     )
 }
--- core/ui/src/main/java/com/massana2110/pokeapp/ui/theme/Theme.kt
+++ core/ui/src/main/java/com/massana2110/pokeapp/ui/theme/Theme.kt
@@ -1,9 +1,12 @@
 package com.massana2110.pokeapp.ui.theme
 
+import android.app.Activity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColorScheme(
     primary = Primary,
@@ -22,6 +25,14 @@ private val LightColorScheme = lightColorScheme(
 fun PocketDexTheme(
     content: @Composable () -> Unit
 ) {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+        }
+    }
+
     MaterialTheme(
         colorScheme = LightColorScheme,
         typography = PocketDexTypography,
